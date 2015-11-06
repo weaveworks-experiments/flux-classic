d2fabf9681bfe6548a3b73890d863a05b933921c
David Wragg <david@wragg.org>
2015-11-06 17:03:26 +0000

    Decouple http request and response handling

--- balancer/interceptor/http.go
+++ balancer/interceptor/http.go
@@ -11,43 +11,99 @@ import (
 )
 
 func httpShim(inbound, outbound *net.TCPConn, eh events.Handler) error {
-	reqrd := bufio.NewReader(inbound)
-	resprd := bufio.NewReader(outbound)
 	defer inbound.Close()
 	defer outbound.Close()
 
 	inboundAddr := inbound.RemoteAddr().(*net.TCPAddr)
 	outboundAddr := outbound.RemoteAddr().(*net.TCPAddr)
 
-	for {
-		// XXX timeout on no request
-		req, err := http.ReadRequest(reqrd)
-		t1 := time.Now()
-		if err != nil {
-			if err == io.EOF {
-				return nil
+	// Request handling and response handling take place in
+	// separate goroutines.  This is not to support pipelining
+	// (although it could easily be extended to do so).  Rather,
+	// it is to support cases where the server produces a response
+	// before the client is done sending the request (e.g. when
+	// the server produces an error response before reading the
+	// whole request).
+
+	type request struct {
+		req      *http.Request
+		tReadReq time.Time
+		err      error
+	}
+
+	reqCh := make(chan request)
+	doneCh := make(chan struct{})
+	defer close(doneCh)
+
+	passReq := func(req request) bool {
+		select {
+		case reqCh <- req:
+			return true
+		case <-doneCh:
+			return false
+		}
+	}
+
+	// The request handler loop
+	go func() {
+		reqrd := bufio.NewReader(inbound)
+		for {
+			// XXX timeout on no request
+			req, err := http.ReadRequest(reqrd)
+			if err != nil {
+				if err == io.EOF {
+					close(reqCh)
+					return
+				}
+
+				passReq(request{err: err})
+				return
 			}
 
-			return err
+			if !passReq(request{req: req, tReadReq: time.Now()}) {
+				return
+			}
+
+			err = req.Write(outbound)
+			if err != nil {
+				passReq(request{err: err})
+				return
+			}
+		}
+	}()
+
+	// The response handler loop
+	resprd := bufio.NewReader(outbound)
+	for {
+		req, ok := <-reqCh
+		if !ok {
+			return nil
 		}
 
-		req.Write(outbound)
-		resp, err := http.ReadResponse(resprd, req)
-		t2 := time.Now()
+		if req.err != nil {
+			return req.err
+		}
+
+		resp, err := http.ReadResponse(resprd, req.req)
+		tReadResponse := time.Now()
 		if err != nil {
 			return err
 		}
 
-		resp.Write(inbound)
-		t3 := time.Now()
+		err = resp.Write(inbound)
+		if err != nil {
+			return err
+		}
+
+		tWroteResponse := time.Now()
 
 		eh.HttpExchange(&events.HttpExchange{
 			Inbound:   inboundAddr,
 			Outbound:  outboundAddr,
-			Request:   req,
+			Request:   req.req,
 			Response:  resp,
-			RoundTrip: t2.Sub(t1),
-			TotalTime: t3.Sub(t1),
+			RoundTrip: tReadResponse.Sub(req.tReadReq),
+			TotalTime: tWroteResponse.Sub(req.tReadReq),
 		})
 	}
 }

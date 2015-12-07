56522a6e791fcb648a75f35265f1571ef72b342c
David Wragg <david@wragg.org>
2015-12-07 13:18:34 +0000

    More testing of common/backends

--- common/backends/etcd_test.go
+++ common/backends/etcd_test.go
@@ -24,29 +24,87 @@ func TestEtcd(t *testing.T) {
 	require.Nil(t, be.Ping())
 }
 
-func TestAddService(t *testing.T) {
+var testService = data.Service{
+	Address:  "1.2.3.4",
+	Port:     1234,
+	Protocol: "tcp",
+	InstanceSpecs: map[data.InstanceGroup]data.InstanceSpec{
+		"group": {
+			AddressSpec: data.AddressSpec{
+				Type: "foo",
+				Port: 5678,
+			},
+			Selector: data.Selector{
+				"foo": "bar",
+			},
+		},
+	},
+}
+
+func TestServices(t *testing.T) {
 	etcd, be := startEtcd(t)
 	defer func() { require.Nil(t, etcd.Destroy()) }()
 
-	svc := data.Service{
-		Address:  "1.2.3.4",
-		Port:     1234,
-		Protocol: "tcp",
-		InstanceSpecs: map[data.InstanceGroup]data.InstanceSpec{
-			"group": {
-				AddressSpec: data.AddressSpec{
-					Type: "foo",
-					Port: 5678,
-				},
-				Selector: data.Selector{
-					"foo": "bar",
-				},
-			},
-		},
-	}
-
-	require.Nil(t, be.AddService("svc", svc))
+	require.Nil(t, be.AddService("svc", testService))
 	svc2, err := be.GetServiceDetails("svc")
 	require.Nil(t, err)
-	require.Equal(t, svc, svc2)
+	require.Equal(t, testService, svc2)
+
+	require.Nil(t, be.CheckRegisteredService("svc"))
+
+	services := func() map[string]data.Service {
+		svcs := make(map[string]data.Service)
+		require.Nil(t, be.ForeachServiceInstance(func(n string, s data.Service) {
+			svcs[n] = s
+		}, nil))
+		return svcs
+	}
+
+	require.Equal(t, map[string]data.Service{"svc": testService}, services())
+
+	require.Nil(t, be.RemoveService("svc"))
+	require.Equal(t, map[string]data.Service{}, services())
+
+	require.Nil(t, be.AddService("svc", testService))
+	require.Nil(t, be.RemoveAllServices())
+	require.Equal(t, map[string]data.Service{}, services())
+}
+
+var testInst = data.Instance{
+	InstanceGroup: "group",
+	Address:       "1.2.3.4",
+	Port:          12345,
+	Labels:        map[string]string{"key": "val"},
+}
+
+func TestInstances(t *testing.T) {
+	etcd, be := startEtcd(t)
+	defer func() { require.Nil(t, etcd.Destroy()) }()
+
+	require.Nil(t, be.AddService("svc", testService))
+	require.Nil(t, be.AddInstance("svc", "inst", testInst))
+
+	instances := func() map[string]data.Instance {
+		insts := make(map[string]data.Instance)
+		require.Nil(t, be.ForeachInstance("svc", func(n string, inst data.Instance) {
+			insts[n] = inst
+		}))
+		return insts
+	}
+
+	require.Equal(t, map[string]data.Instance{"inst": testInst}, instances())
+
+	serviceInstances := func() map[string]data.Instance {
+		insts := make(map[string]data.Instance)
+		require.Nil(t, be.ForeachServiceInstance(nil, func(n string, inst data.Instance) {
+			insts[n] = inst
+		}))
+		return insts
+	}
+
+	require.Equal(t, map[string]data.Instance{"inst": testInst}, serviceInstances())
+
+	require.Nil(t, be.RemoveInstance("svc", "inst"))
+	require.Equal(t, map[string]data.Instance{}, instances())
+	require.Equal(t, map[string]data.Instance{}, serviceInstances())
 }

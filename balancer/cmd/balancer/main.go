9bd714adb545abd5eaeb19c36a4d897fe4a9f41d
David Wragg <david@wragg.org>
2015-12-08 13:42:09 +0000

    Improve signal handling in balancer

--- balancer/cmd/balancer/main.go
+++ balancer/cmd/balancer/main.go
@@ -16,20 +16,33 @@ func iptables(args []string) ([]byte, error) {
 }
 
 func main() {
-	exitCode := 0
-	defer os.Exit(exitCode)
-
+	// Catch some signals for whcih we want to clean up on exit
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
 	fatalSink := fatal.New()
 	i := balancer.Start(os.Args, fatalSink, iptables)
-	defer i.Stop()
+
+	exitCode := 0
+	var exitSignal os.Signal
 
 	select {
-	case <-sigs:
 	case err := <-fatalSink:
 		fmt.Fprintln(os.Stderr, err)
 		exitCode = 1
+	case exitSignal = <-sigs:
+		exitCode = 2
 	}
+
+	i.Stop()
+
+	if sig, ok := exitSignal.(syscall.Signal); ok {
+		// Now we have cleaned up, re-kill the process with
+		// the signal in order to produce a signal exit
+		// status:
+		signal.Reset(sig)
+		syscall.Kill(syscall.Getpid(), sig)
+	}
+
+	os.Exit(exitCode)
 }

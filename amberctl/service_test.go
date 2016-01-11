cdc3e2fa6ad02d4b9952de8d37abfdc811cdce13
Michael Bridgen <mikeb@squaremobius.net>
2016-01-11 13:10:26 +0000

    More tests for service subcommand

--- amberctl/service_test.go
+++ amberctl/service_test.go
@@ -70,7 +70,7 @@ func TestParseAddress(t *testing.T) {
 
 }
 
-func TestAddress(t *testing.T) {
+func TestServiceAddress(t *testing.T) {
 	st, err := runCmd([]string{
 		"foo", "--address", "10.3.4.5:8000"})
 	require.NoError(t, err)
@@ -79,4 +79,29 @@ func TestAddress(t *testing.T) {
 	require.Equal(t, "foo", services[0].Name)
 	require.Equal(t, "10.3.4.5", services[0].Address)
 	require.Equal(t, 8000, services[0].Port)
+}
+
+func TestServiceSelectMissingPortSpec(t *testing.T) {
+	_, err := runCmd([]string{
+		"svc", "--image", "repo/image",
+	})
+	require.Error(t, err)
+}
+
+func TestServiceSelect(t *testing.T) {
+	st, err := runCmd([]string{
+		"svc", "--image", "repo/image", "--port-fixed", "9000",
+	})
+	require.NoError(t, err)
+	services := allServices(st)
+	require.Len(t, services, 1)
+	specs, err := st.GetContainerGroupSpecs("svc")
+	require.NoError(t, err)
+	require.Len(t, specs, 1)
+	spec := specs[DEFAULT_GROUP]
+	require.NotNil(t, spec)
+	require.Equal(t, data.Selector(map[string]string{
+		"image": "repo/image",
+	}), spec.Selector)
+	require.Equal(t, data.AddressSpec{"fixed", 9000}, spec.AddressSpec)
 }

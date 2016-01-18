9978b96650a5363024d71d6d1b1873cf72f541ce
David Kaltschmidt <david.kaltschmidt@gmail.com>
2016-01-18 11:28:40 +0100

    Build js bundle in /build

--- web/local.mk
+++ web/local.mk
@@ -4,10 +4,16 @@ WEB_STATIC:=web/src/index.html
 
 $(call image_stamp,web): $(WEB_STATIC) $(WEB_BUILD) web/build/app.js web/webpack.production.config.js
 
-$(call image_stamp,webbuild): web/package.json
+$(call image_stamp,webbuild): web/package.json web/.babelrc web/.eslintrc web/.eslintignore web/webpack.production.config.js
+
+#web/build/app.js: $(call image_stamp,webbuild)
+#	$(call run_build_container,webbuild,,web,npm run build)
 
 web/build/app.js: $(call image_stamp,webbuild)
-	$(call run_build_container,webbuild,,web,npm run build)
+	mkdir -p web/build
+	docker run --rm -v $(shell pwd)/web/src:/build/src \
+		-v $(shell pwd)/web/build:/build/build \
+		$(call docker_tag,webbuild) npm run build
 
 .PHONY: clean-web
 clean:: clean-web

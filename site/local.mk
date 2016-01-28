bfe22d443181dbcbb993d31001bb04b032a2426b
Michael Bridgen <mikeb@squaremobius.net>
2016-01-28 12:27:32 +0000

    Use a <base> element to work locally and in gh-pages
    Requirements:
     - http://localhost:4000/fluxctl works
     - http://weaveworks.github.io/flux/fluxctl works
     - (would be nice) http://.../fluxctl/ works
    
    Using a <base> element means that any relative URLs are resolved
    relative to the given URL, so we have to be careful to remember to
    start URLs 'at the top'.
    
    NB that the baseurl in _config.yml is for the gh-pages deployment, and
    is overridden if you run the site with the makefile.

--- site/local.mk
+++ site/local.mk
@@ -5,4 +5,4 @@ run-site: $(call image_stamp,site)
 	docker run --rm -v $$PWD/site:/site \
 		-v $$PWD/docker/build-wrapper.sh:/build-wrapper.sh \
 		--workdir=/site -p 4000:4000 \
-		$(call docker_tag,site) sh /build-wrapper.sh "bundler install && bundle exec jekyll serve --host 0.0.0.0"
+		$(call docker_tag,site) sh /build-wrapper.sh "bundler install && bundle exec jekyll serve --host 0.0.0.0 --baseurl=''"

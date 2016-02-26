8862f24776793270810a867d3a5718607c46dd94
David Kaltschmidt <david.kaltschmidt@gmail.com>
2016-02-26 16:18:21 +0100

    Render charts inline of instance component
    * always render chart, no clicks
    * render chart below hero metric and metadata

--- web/src/components/instance-group.js
+++ web/src/components/instance-group.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classnames from 'classnames';
 
+import PrometheusChart from './charts/prometheus-chart';
 import { formatMetric, plural } from '../utils/string-utils';
 
 export default class InstanceGroup extends React.Component {
@@ -8,6 +9,7 @@ export default class InstanceGroup extends React.Component {
     const { group, heroMetrics } = this.props;
     const count = group.length;
     const instance = group[0];
+    const instanceSpec = group.map(ins => ins.name);
     const name = `${instance.labels.image}:${instance.labels.tag}`;
     const metrics = group.map(ins => heroMetrics.get(ins.name)).filter(val => val !== undefined);
     const avg = metrics.length ? formatMetric(metrics.reduce((res, val) => res + val) / metrics.length) : 'n/a';
@@ -20,32 +22,37 @@ export default class InstanceGroup extends React.Component {
 
     return (
       <div className={className} onClick={this.props.handleClick}>
-        <div className="instance-metric">
-          {/* this should be the group avg */}
-          <span className="instance-metric-value">{avg}</span>
-          <span className="instance-metric-unit">QPS</span>
-          <div className="instance-metric-aggregate-wrap">
-            <div className="instance-metric-aggregate">
-              <span className="instance-metric-aggregate-icon fa fa-sort-up" />
-              <span className="instance-metric-aggregate-value">{max}</span>
+        <div className="instance-info">
+          <div className="instance-metric">
+            {/* this should be the group avg */}
+            <span className="instance-metric-value">{avg}</span>
+            <span className="instance-metric-unit">QPS</span>
+            <div className="instance-metric-aggregate-wrap">
+              <div className="instance-metric-aggregate">
+                <span className="instance-metric-aggregate-icon fa fa-sort-up" />
+                <span className="instance-metric-aggregate-value">{max}</span>
+              </div>
+              <div className="instance-metric-aggregate">
+                <span className="instance-metric-aggregate-icon fa fa-sort-down" />
+                <span className="instance-metric-aggregate-value">{min}</span>
+              </div>
             </div>
-            <div className="instance-metric-aggregate">
-              <span className="instance-metric-aggregate-icon fa fa-sort-down" />
-              <span className="instance-metric-aggregate-value">{min}</span>
+          </div>
+          <div className="instance-other">
+            <div className="instance-title truncate" title={'Name: ' + name}>
+              {name}
+            </div>
+            <div className="instance-other-field">
+              {count}
+              {' '}
+              <span className="instance-other-field-label">
+                {plural('instance', count)}
+              </span>
             </div>
           </div>
         </div>
-        <div className="instance-title truncate" title={'Name: ' + name}>
-          {name}
-        </div>
-        <div className="instance-other">
-          <div className="instance-other-field">
-            {count}
-            {' '}
-            <span className="instance-other-field-label">
-              {plural('instance', count)}
-            </span>
-          </div>
+        <div className="instance-chart">
+          <PrometheusChart spec={{individual: instanceSpec}}/>
         </div>
       </div>
     );

64ae06dcbbab7e86cff4eae9055ef2afe01531ea
David Wragg <david@wragg.org>
2016-03-03 11:17:27 +0000

    A restartable docker listener

---
+++ agent/docker_listener.go
@@ -0,0 +1,263 @@
+package agent
+
+import (
+	"sync"
+
+	log "github.com/Sirupsen/logrus"
+	docker "github.com/fsouza/go-dockerclient"
+
+	"github.com/weaveworks/flux/common/daemon"
+)
+
+type ContainerUpdate struct {
+	Containers map[string]*docker.Container
+	Reset      bool
+}
+
+type dockerListener struct {
+	client  dockerClient
+	stop    chan struct{}
+	stopped sync.Once
+}
+
+type dockerClient interface {
+	AddEventListener(listener chan<- *docker.APIEvents) error
+	RemoveEventListener(listener chan *docker.APIEvents) error
+	ListContainers(opts docker.ListContainersOptions) ([]docker.APIContainers, error)
+	InspectContainer(id string) (*docker.Container, error)
+}
+
+// Passed from the readContainerIDs goroutine to the inspectContainers
+// goroutine, via the buffer goroutine
+type containerIDs struct {
+	containers map[string]bool
+	reset      bool
+}
+
+func NewDockerListener(out chan<- ContainerUpdate) daemon.StartFunc {
+	return func(es daemon.ErrorSink) daemon.Component {
+		dl := &dockerListener{stop: make(chan struct{})}
+
+		go func() {
+			if err := dl.start(es, out); err != nil {
+				es.Post(err)
+			}
+		}()
+
+		return dl
+	}
+}
+
+func (dl *dockerListener) Stop() {
+	dl.stopped.Do(func() {
+		close(dl.stop)
+	})
+}
+
+func (dl *dockerListener) start(es daemon.ErrorSink, out chan<- ContainerUpdate) error {
+	client, err := docker.NewClientFromEnv()
+	if err != nil {
+		return err
+	}
+
+	env, err := client.Version()
+	if err != nil {
+		return err
+	}
+	log.Infof("Using Docker %+v", env)
+
+	dl.client = client
+	return dl.startAux(es, out)
+}
+
+func (dl *dockerListener) startAux(es daemon.ErrorSink, out chan<- ContainerUpdate) error {
+	bufContainerIDs := make(chan containerIDs)
+	containerIDs := make(chan containerIDs)
+
+	go func() {
+		if err := dl.readContainerIDs(bufContainerIDs); err != nil {
+			es.Post(err)
+		}
+	}()
+
+	// Buffer the container ID updates, because if inspecting a
+	// container takes a long time, it shouldn't block handling of
+	// events from the docker client.
+	go dl.bufferContainerIDs(bufContainerIDs, containerIDs)
+	return dl.inspectContainers(containerIDs, out)
+}
+
+func (dl *dockerListener) readContainerIDs(out chan<- containerIDs) error {
+	// We need to start listening for events before we list the
+	// existing containers
+	events := make(chan *docker.APIEvents)
+	if err := dl.client.AddEventListener(events); err != nil {
+		return err
+	}
+
+	defer dl.client.RemoveEventListener(events)
+
+	// Asynchronously retrieve the list of containers
+	var listContainersRes []docker.APIContainers
+	listContainersCh := make(chan error, 1)
+	go func() {
+		defer close(listContainersCh)
+		var err error
+		listContainersRes, err = dl.client.ListContainers(docker.ListContainersOptions{})
+		listContainersCh <- err
+	}()
+
+	containers := make(map[string]bool)
+whileListingContainers:
+	for {
+		select {
+		case err := <-listContainersCh:
+			if err != nil {
+				return err
+			}
+
+			break whileListingContainers
+
+		case ev := <-events:
+			switch ev.Status {
+			case "start":
+				containers[ev.ID] = true
+			case "die":
+				containers[ev.ID] = false
+			}
+
+		case <-dl.stop:
+			return nil
+		}
+
+	}
+
+	for _, c := range listContainersRes {
+		if _, present := containers[c.ID]; !present {
+			containers[c.ID] = true
+		}
+	}
+
+	// Remove the IDs of any containers that have died.  We do
+	// this even is they were in the results of ListContainers,
+	// because the "die" event must mean they have gone.
+	for id, started := range containers {
+		if !started {
+			delete(containers, id)
+		}
+	}
+
+	out <- containerIDs{containers: containers, reset: true}
+
+	// Handle further events
+	for {
+		var ev *docker.APIEvents
+		select {
+		case ev = <-events:
+		case <-dl.stop:
+			return nil
+		}
+
+		var started bool
+		switch ev.Status {
+		case "start":
+			started = true
+		case "die":
+			started = false
+		default:
+			continue
+		}
+
+		select {
+		case out <- containerIDs{
+			containers: map[string]bool{ev.ID: started},
+		}:
+		case <-dl.stop:
+			return nil
+		}
+
+	}
+}
+
+func (dl *dockerListener) bufferContainerIDs(in <-chan containerIDs, out chan<- containerIDs) {
+	var buf []containerIDs
+
+	for {
+		if len(buf) == 0 {
+			select {
+			case ids := <-in:
+				buf = append(buf, ids)
+			case <-dl.stop:
+				return
+			}
+		} else {
+			select {
+			case ids := <-in:
+				buf = append(buf, ids)
+			case out <- buf[0]:
+				buf = buf[1:]
+			case <-dl.stop:
+				return
+			}
+		}
+	}
+}
+
+func (dl *dockerListener) inspectContainers(in <-chan containerIDs, out chan<- ContainerUpdate) error {
+	// It's possible that we'll hear about a container being
+	// started, but by the time we inspect it, it's already gone.
+	// In such a case, we want to suppress both the arrival and
+	// the disappearance of the container.  So we have to keep
+	// track of which containers we have announced.
+	announced := make(map[string]struct{})
+
+	for {
+		var inUpdate containerIDs
+		select {
+		case inUpdate = <-in:
+		case <-dl.stop:
+			return nil
+		}
+
+		if inUpdate.reset {
+			announced = make(map[string]struct{})
+		}
+
+		outUpdate := make(map[string]*docker.Container)
+		for id, started := range inUpdate.containers {
+			if !started {
+				if _, present := announced[id]; present {
+					outUpdate[id] = nil
+					delete(announced, id)
+				}
+
+				continue
+			}
+
+			details, err := dl.client.InspectContainer(id)
+			if err != nil {
+				if _, noSuch := err.(*docker.NoSuchContainer); noSuch {
+					continue
+				}
+
+				return err
+			}
+
+			outUpdate[id] = details
+			announced[id] = struct{}{}
+		}
+
+		if len(outUpdate) == 0 {
+			continue
+		}
+
+		select {
+		case out <- ContainerUpdate{
+			Containers: outUpdate,
+			Reset:      inUpdate.reset,
+		}:
+		case <-dl.stop:
+			return nil
+		}
+	}
+}
---
+++ agent/docker_listener_test.go
@@ -0,0 +1,189 @@
+package agent
+
+import (
+	"sync"
+	"testing"
+	"time"
+
+	docker "github.com/fsouza/go-dockerclient"
+	"github.com/stretchr/testify/require"
+
+	"github.com/weaveworks/flux/common/daemon"
+)
+
+type mockDockerClient struct {
+	lock        sync.Mutex
+	containers  map[string]*docker.Container
+	listeners   map[chan<- *docker.APIEvents]struct{}
+	concAdds    []*docker.Container
+	concRemoves []string
+}
+
+func newMockDockerClient() *mockDockerClient {
+	return &mockDockerClient{
+		containers: make(map[string]*docker.Container),
+		listeners:  make(map[chan<- *docker.APIEvents]struct{}),
+	}
+}
+
+func (mdc *mockDockerClient) AddEventListener(listener chan<- *docker.APIEvents) error {
+	mdc.lock.Lock()
+	defer mdc.lock.Unlock()
+	mdc.listeners[listener] = struct{}{}
+	return nil
+}
+
+func (mdc *mockDockerClient) RemoveEventListener(listener chan *docker.APIEvents) error {
+	mdc.lock.Lock()
+	defer mdc.lock.Unlock()
+	delete(mdc.listeners, listener)
+	return nil
+}
+
+func (mdc *mockDockerClient) ListContainers(opts docker.ListContainersOptions) ([]docker.APIContainers, error) {
+	mdc.lock.Lock()
+
+	var res []docker.APIContainers
+	for _, cont := range mdc.containers {
+		res = append(res, docker.APIContainers{
+			ID:    cont.ID,
+			Names: []string{cont.Name},
+		})
+	}
+
+	mdc.lock.Unlock()
+
+	for _, cont := range mdc.concAdds {
+		mdc.addContainer(cont, false)
+	}
+
+	for _, id := range mdc.concRemoves {
+		mdc.removeContainer(id, false)
+	}
+
+	return res, nil
+}
+
+func (mdc *mockDockerClient) InspectContainer(id string) (*docker.Container, error) {
+	mdc.lock.Lock()
+	defer mdc.lock.Unlock()
+
+	res := mdc.containers[id]
+	if res == nil {
+		return nil, &docker.NoSuchContainer{ID: id}
+	}
+
+	return res, nil
+}
+
+func (mdc *mockDockerClient) addContainer(cont *docker.Container, async bool) {
+	mdc.lock.Lock()
+	defer mdc.lock.Unlock()
+
+	mdc.containers[cont.ID] = cont
+	mdc.notify(async, docker.APIEvents{
+		ID:     cont.ID,
+		Status: "start",
+	})
+}
+
+func (mdc *mockDockerClient) removeContainer(id string, async bool) {
+	mdc.lock.Lock()
+	defer mdc.lock.Unlock()
+
+	delete(mdc.containers, id)
+	mdc.notify(async, docker.APIEvents{
+		ID:     id,
+		Status: "die",
+	})
+}
+
+// Generate the events for a container coming and going, but so that
+// it is gone by the time it can be inspected
+func (mdc *mockDockerClient) transientContainer(id string, async bool) {
+	mdc.lock.Lock()
+	defer mdc.lock.Unlock()
+
+	mdc.notify(async, docker.APIEvents{
+		ID:     id,
+		Status: "start",
+	}, docker.APIEvents{
+		ID:     id,
+		Status: "die",
+	})
+}
+
+func (mdc *mockDockerClient) notify(async bool, evs ...docker.APIEvents) {
+	for ch := range mdc.listeners {
+		f := func() {
+			for i := range evs {
+				ch <- &evs[i]
+			}
+		}
+		if async {
+			go f()
+		} else {
+			f()
+		}
+	}
+}
+
+func TestDockerListener(t *testing.T) {
+	mdc := newMockDockerClient()
+	es := daemon.NewErrorSink()
+	updates := make(chan ContainerUpdate)
+	dl := &dockerListener{
+		client: mdc,
+		stop:   make(chan struct{}),
+	}
+
+	mdc.addContainer(&docker.Container{
+		ID:   "1",
+		Name: "/foo",
+	}, true)
+	mdc.addContainer(&docker.Container{
+		ID:   "2",
+		Name: "/bar",
+	}, true)
+	mdc.concRemoves = []string{"2"}
+	mdc.concAdds = []*docker.Container{{
+		ID:   "3",
+		Name: "/qux",
+	}}
+
+	go func() {
+		if err := dl.startAux(es, updates); err != nil {
+			es.Post(err)
+		}
+	}()
+
+	update := <-updates
+	require.True(t, update.Reset)
+	require.Len(t, update.Containers, 2)
+	require.Equal(t, update.Containers["1"].Name, "/foo")
+	require.Equal(t, update.Containers["3"].Name, "/qux")
+
+	mdc.removeContainer("1", true)
+	update = <-updates
+	require.False(t, update.Reset)
+	require.Len(t, update.Containers, 1)
+	require.Nil(t, update.Containers["1"])
+
+	// This shouldn't result in any updates
+	mdc.transientContainer("4", false)
+
+	mdc.addContainer(&docker.Container{
+		ID:   "5",
+		Name: "/qux",
+	}, true)
+	update = <-updates
+	require.False(t, update.Reset)
+	require.Len(t, update.Containers, 1)
+	require.Equal(t, update.Containers["5"].Name, "/qux")
+
+	dl.Stop()
+	// Ugly sleep to let the goroutines stop
+	time.Sleep(10 * time.Millisecond)
+	require.Empty(t, updates)
+	require.Empty(t, es)
+}
